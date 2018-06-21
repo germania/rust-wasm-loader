8769fceb0fc05f46a50651c0819b3282cd598917
Ben Clarke <rod.parfait@gmail.com>
2018-06-20 23:50:06 -0400

    Merge from upstream

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,15 @@
-const wasm = require('./lib.rs');
+async function loadwasm() {
+  const lib = await require('./lib.rs');
+  lib.hello();
+  console.log(lib.doub(21));
+}
 
-wasm.then(module => {
-  console.log('Calling rust functions!');
-  module.hello();
-  console.log(module.doub(21));
-})
+loadwasm();
+
+// const wasm = require('./lib.rs');
+
+// wasm.then(module => {
+//   console.log('Calling rust functions!');
+//   module.hello();
+//   console.log(module.doub(21));
+// })
